222fcb962a20e5dcfd2a836ceb9ee9fb326245a7
smarthug <kirklayer@gmail.com>
2020-12-27 10:10:58 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+import Axios from 'axios'
 
 function App() {
+
+  useEffect(()=>{
+    console.log("hello api");
+
+
+    // success
+    // Axios.get('https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=Ojx1dXXUoWE&key=AIzaSyCFpjtE9aDba5P1vuLN4-pfbERNetQTK7I')
+    // .then((res)=>{
+    //   console.log(res)
+    // })
+
+    Axios.get('https://youtube.googleapis.com/youtube/v3/comments?part=snippet&id=UgxTZoAsM56HotDOra54AaABAg&key=AIzaSyCFpjtE9aDba5P1vuLN4-pfbERNetQTK7I')
+    .then((res)=>{
+      console.log(res)
+    })
+
+    //UgxTZoAsM56HotDOra54AaABAg
+
+    Axios.get('https://youtube.googleapis.com/youtube/v3/comments?part=snippet&id=UgxTZoAsM56HotDOra54AaABAg&key=AIzaSyCFpjtE9aDba5P1vuLN4-pfbERNetQTK7I')
+    .then((res)=>{
+      console.log(res)
+    })
+
+
+  },[])
   return (
     <div className="App">
       <header className="App-header">
